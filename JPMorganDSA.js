509a941eb4bddc209e1b0cfac8ee82ad34cc27b1
unknown <singhxamit21@example.com>
2025-10-12 12:11:17 +0530

    work in progress

--- JPMorganDSA.js
+++ JPMorganDSA.js
@@ -25,14 +25,14 @@ function reverseString(str) {
 
 //Reverse a Array
 const reverseArray = (s) => {
-    let len = s.length;
-    let halfLen = Math.floor(len / 2);
+  let len = s.length;
+  let halfLen = Math.floor(len / 2);
 
-    for (let i = 0; i < halfLen; i++) {
-        let temp = s[i];
-        s[i] = s[len - i - 1];
-        s[len - i - 1] = temp;
-    }
+  for (let i = 0; i < halfLen; i++) {
+    let temp = s[i];
+    s[i] = s[len - i - 1];
+    s[len - i - 1] = temp;
+  }
 
 };
 
@@ -40,25 +40,25 @@ console.log(reverseArray(["h", "e", "l", "l", "o"])) //["o", "l", "l", "e", "h"]
 
 //Palindrome Check
 var isPalindrome = function (s) {
-    s = s.toLowerCase();
-    let i = 0;
-    let j = s.length - 1;
-    while (i < j) {
-        if (!s[i].match(/[a-z0-9]/i)) {
-            ++i;
-        }
-        else if (!s[j].match(/[a-z0-9]/i)) {
-            --j;
-        }
-        else if (s[i] === s[j]) {
-            ++i;
-            --j;
-        }
-        else {
-            return false;
-        }
+  s = s.toLowerCase();
+  let i = 0;
+  let j = s.length - 1;
+  while (i < j) {
+    if (!s[i].match(/[a-z0-9]/i)) {
+      ++i;
     }
-    return true;
+    else if (!s[j].match(/[a-z0-9]/i)) {
+      --j;
+    }
+    else if (s[i] === s[j]) {
+      ++i;
+      --j;
+    }
+    else {
+      return false;
+    }
+  }
+  return true;
 };
 
 //FizzBuzz
@@ -82,15 +82,15 @@ fizzBuzz(15);
 // Example: [3,0,1] → 2
 
 const missingNumber = (nums) => {
-    let n = nums.length;
-    let total_sum = (n * (n + 1)) / 2;
-    let sum_of_array = 0;
+  let n = nums.length;
+  let total_sum = (n * (n + 1)) / 2;
+  let sum_of_array = 0;
 
-    for (let num of nums) {
-        sum_of_array += num;
-    }
+  for (let num of nums) {
+    sum_of_array += num;
+  }
 
-    return total_sum - sum_of_array;
+  return total_sum - sum_of_array;
 };
 
 console.log(missingNumber([3, 0, 1])) //2
@@ -178,34 +178,34 @@ console.log(isValidParenthesis("(")); // false
 
 //Merge Two Sorted Arrays
 const OptimalMerge = (nums1, m, nums2, n) => {
-    let p1 = m - 1; // Last meaningful element of nums1
-    let p2 = n - 1; // Last element of nums2
+  let p1 = m - 1; // Last meaningful element of nums1
+  let p2 = n - 1; // Last element of nums2
 
-    for (let i = m + n - 1; i >= 0; i--) {
-        if (p2 < 0) break; // If nums2 is done, any remaining elements in nums1 are already in the correct place because the beginning of nums1 is already sorted.
+  for (let i = m + n - 1; i >= 0; i--) {
+    if (p2 < 0) break; // If nums2 is done, any remaining elements in nums1 are already in the correct place because the beginning of nums1 is already sorted.
 
-        if (p1 >= 0 && nums1[p1] > nums2[p2]) { 
-            nums1[i] = nums1[p1--];
-        } else {
-            nums1[i] = nums2[p2--];
-        }
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1--];
+    } else {
+      nums1[i] = nums2[p2--];
     }
+  }
 };
 
 console.log(OptimalMerge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3)) //[1,2,2,3,5,6]
-console.log(OptimalMerge([1,2,3,0,0,0], 3, [4,5,6], 3)) //[1,2,3,4,5,6] //nums2 gets fully used before nums1
+console.log(OptimalMerge([1, 2, 3, 0, 0, 0], 3, [4, 5, 6], 3)) //[1,2,3,4,5,6] //nums2 gets fully used before nums1
 
 //Count Occurrences
 // [1,2,2,3,3,3] → {1:1, 2:2, 3:3}
 const countOccurrences = (arr) => {
-    const counts = {};
+  const counts = {};
 
-    for (let num of arr) {
-        // If num exists in counts, increment it; otherwise, initialize to 1
-        counts[num] = (counts[num] || 0) + 1;
-    }
+  for (let num of arr) {
+    // If num exists in counts, increment it; otherwise, initialize to 1
+    counts[num] = (counts[num] || 0) + 1;
+  }
 
-    return counts;
+  return counts;
 };
 
 // Example
@@ -215,21 +215,21 @@ console.log(result); // { '1': 1, '2': 2, '3': 3 }
 
 //Longest Substring Without Repeating Characters
 const lengthOfLongestSubstring = (s) => {
-    let set = new Set();
-    let left = 0, maxLength = 0;
+  let set = new Set();
+  let left = 0, maxLength = 0;
 
-    for (let right = 0; right < s.length; right++) {
-        // If character is already in the set, remove from left until it's gone
-        while (set.has(s[right])) {
-            set.delete(s[left]);
-            left++;
-        }
-
-        set.add(s[right]);
-        maxLength = Math.max(maxLength, right - left + 1);
+  for (let right = 0; right < s.length; right++) {
+    // If character is already in the set, remove from left until it's gone
+    while (set.has(s[right])) {
+      set.delete(s[left]);
+      left++;
     }
 
-    return maxLength;
+    set.add(s[right]);
+    maxLength = Math.max(maxLength, right - left + 1);
+  }
+
+  return maxLength;
 };
 
 // Examples
@@ -239,30 +239,53 @@ console.log(lengthOfLongestSubstring("pwwkew"));   // 3
 
 
 //Group Anagrams : ["eat","tea","tan","ate","nat","bat"] → [["eat","tea","ate"],["tan","nat"],["bat"]]
-var groupAnagrams = function(strs) {
-    let map = {};
+var groupAnagrams = function (strs) {
+  let map = {};
 
-    for (let s of strs) {
-        // frequency array for 'a' to 'z'
-        let freqArr = Array(26).fill(0);
+  for (let s of strs) {
+    // frequency array for 'a' to 'z'
+    let freqArr = Array(26).fill(0);
 
-        for (let ch of s) {
-            let index = ch.charCodeAt(0) - "a".charCodeAt(0);
-            freqArr[index]++;
-        }
-
-        // use the frequency array as a unique key
-        let key = freqArr.join(",");
-
-        if (map[key]) {
-            map[key].push(s);
-        } else {
-            map[key] = [s];
-        }
+    for (let ch of s) {
+      let index = ch.charCodeAt(0) - "a".charCodeAt(0);
+      freqArr[index]++;
     }
 
-    return Object.values(map);
+    // use the frequency array as a unique key
+    let key = freqArr.join(",");
+
+    if (map[key]) {
+      map[key].push(s);
+    } else {
+      map[key] = [s];
+    }
+  }
+
+  return Object.values(map);
 };
+
+// fibonacci
+const fibonacci = (n) => {
+  if (n <= 1) return n;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+};
+
+console.log(fibonacci(5)) //5
+console.log(fibonacci(8)) //21
+
+const fibonacciSequence = (number) => {
+  let n1 = 0, n2 = 1, nextTerm;
+  let sequence = [];
+  for (let i = 1; i <= number; i++) {
+    sequence.push(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+  }
+  return sequence;
+};
+
+console.log(fibonacciSequence(5));
 
 //Rotate Matrix (2D array) [[1,2],[3,4]] → [[3,1],[4,2]]
 function rotate(matrix) {
