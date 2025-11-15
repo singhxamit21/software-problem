59803f454c5b8cf12f5931d8938efb2cb0f7340f
unknown <singhxamit21@example.com>
2025-11-15 19:23:44 +0530

    work in progress

--- Array/challenges.js
+++ Array/challenges.js
@@ -491,6 +491,29 @@ const sortWithOutInbuild = (array) => {
 console.log(sortWithOutInbuild([10, 2, 30, 1]))
 console.log(sortWithOutInbuild(["banana", "apple", "cherry"]))
 
+//Merge Sort
+var sortArray = function(nums) {
+    if (nums.length <= 1) return nums;
+    let mid = Math.floor(nums.length / 2);
+    let left = sortArray(nums.slice(0, mid));
+    let right = sortArray(nums.slice(mid));
+    return merge(left, right);
+};
+
+function merge(left, right) {
+    let res = [], i = 0, j = 0;
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            res.push(left[i++]);
+        } else {
+            res.push(right[j++]);
+        }
+    }
+    return [...res, ...left.slice(i), ...right.slice(j)];
+}
+
+console.log("Merge Sort array", insertionSort([5, 2, 4, 1]));
+
 function groupAnagrams(strs) {
   const anagramMap = {};
 
